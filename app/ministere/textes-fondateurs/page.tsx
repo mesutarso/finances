8d0378b3b65383a987ef0e51234c826d67fcc3f5
mechack tambwe <mesutarso@users.noreply.github.com>
2025-04-14 12:27:13 +0100

    fix issues

--- app/ministere/textes-fondateurs/page.tsx
+++ app/ministere/textes-fondateurs/page.tsx
@@ -1,6 +1,6 @@
-import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+
 import { texteFondateurQuery } from "@/lib/react-query/ministere/options";
-import { getQueryClient } from "@/components/providers/react-query/client";
+
 import Attributions from "@/components/ministere/attributions";
 import { Metadata } from "next";
 
@@ -12,24 +12,23 @@ export const metadata: Metadata = {
 export const dynamic = "force-dynamic";
 
 export default async function TextesFondateurs() {
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(texteFondateurQuery);
+
 
 
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="container section">
-        <div className="mb-8 text-center">
-          <h1 className="text-3xl font-bold mb-2 text-primary">Textes Fondateurs</h1>
-          <h2 className="text-xl font-bold mb-2">Ministère des Finances</h2>
-          <p className="text-xl text-muted-foreground">
-            République Démocratique du Congo
-          </p>
-        </div>
-        <Attributions />
+
+    <div className="container section">
+      <div className="mb-8 text-center">
+        <h1 className="text-3xl font-bold mb-2 text-primary">Textes Fondateurs</h1>
+        <h2 className="text-xl font-bold mb-2">Ministère des Finances</h2>
+        <p className="text-xl text-muted-foreground">
+          République Démocratique du Congo
+        </p>
       </div>
-    </HydrationBoundary>
+      <Attributions />
+    </div>
+
   );
 }
 
--- components/ministere/attributions.tsx
+++ components/ministere/attributions.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { texteFondateurQuery } from "@/lib/react-query/ministere/options";
 
 const Attributions = () => {
-    const { data } = useSuspenseQuery(texteFondateurQuery);
+    const { data } = useQuery(texteFondateurQuery);
     return <div className="container section prose attributions">
-        <div className="prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-p:text-base prose-a:text-primary prose-ul:list-disc prose-ol:list-decimal" dangerouslySetInnerHTML={{ __html: data.contenu }} />
+        <div className="prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-p:text-base prose-a:text-primary prose-ul:list-disc prose-ol:list-decimal" dangerouslySetInnerHTML={{ __html: data?.contenu }} />
     </div>
 
 };
