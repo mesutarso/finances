247388f5fc8f594e7eed3721f5c9eb01b97f71f8
mechack tambwe <mesutarso@users.noreply.github.com>
2025-04-14 12:22:01 +0100

    fix issues

--- app/ministere/textes-fondateurs/page.tsx
+++ app/ministere/textes-fondateurs/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: "Textes Fondateurs",
 }
 
-export const revalidate = 3600;
+export const dynamic = "force-dynamic";
 
 export default async function TextesFondateurs() {
   const queryClient = getQueryClient();
